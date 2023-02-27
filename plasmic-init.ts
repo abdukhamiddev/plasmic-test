7cb80ba7fa1d05302ead1e0a3ecba879fa4a5eb9
abdukhamiddev <latipovabdukhamid@gmail.com>
2023-02-27 09:18:23 +0500

    fixing deploy issue

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -29,40 +29,40 @@ export const PLASMIC = initPlasmicLoader({
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 import { registerAll } from "@plasmicpkgs/plasmic-sanity-io";
 
-import { registerAll as registerWodpress } from "@plasmicpkgs/plasmic-wordpress";
-import { registerAll as registerRevue } from "@plasmicpkgs/plasmic-revue";
-import { registerAll as ContentfulRegister } from "@plasmicpkgs/plasmic-contentful";
-import { registerAll as ContentstackRegister } from "@plasmicpkgs/plasmic-contentstack";
-import { registerAll as registerStrapi } from "@plasmicpkgs/plasmic-strapi";
-import { registerAll as registerPigeon } from "@plasmicpkgs/plasmic-pigeon-maps";
-import { registerAll as registerTypeform } from "@plasmicpkgs/plasmic-typeform";
-import { registerAll as registerCalendly } from "@plasmicpkgs/plasmic-calendly";
+// import { registerAll as registerWodpress } from "@plasmicpkgs/plasmic-wordpress";
+// import { registerAll as registerRevue } from "@plasmicpkgs/plasmic-revue";
+// import { registerAll as ContentfulRegister } from "@plasmicpkgs/plasmic-contentful";
+// import { registerAll as ContentstackRegister } from "@plasmicpkgs/plasmic-contentstack";
+// import { registerAll as registerStrapi } from "@plasmicpkgs/plasmic-strapi";
+// import { registerAll as registerPigeon } from "@plasmicpkgs/plasmic-pigeon-maps";
+// import { registerAll as registerTypeform } from "@plasmicpkgs/plasmic-typeform";
+// import { registerAll as registerCalendly } from "@plasmicpkgs/plasmic-calendly";
 import { registerSlider as registerKeen } from "@plasmicpkgs/keen-slider";
-import { registerAll as googleMaps } from "@plasmicpkgs/plasmic-google-maps";
-import { registerAll as mailchimp } from "@plasmicpkgs/plasmic-mailchimp";
-import { registerAll as hubspot } from "@plasmicpkgs/plasmic-hubspot";
-import { registerAll as eventbrite } from "@plasmicpkgs/plasmic-eventbrite";
-import { registerAll as soundcloud } from "@plasmicpkgs/plasmic-soundcloud";
-import { registerAll as giphy } from "@plasmicpkgs/plasmic-giphy";
-import { registerAll as spotify } from "@plasmicpkgs/plasmic-spotify";
-import { registerAll as intercom } from "@plasmicpkgs/plasmic-intercom";
-import { registerAll as audio } from "@plasmicpkgs/plasmic-audio";
+// import { registerAll as googleMaps } from "@plasmicpkgs/plasmic-google-maps";
+// import { registerAll as mailchimp } from "@plasmicpkgs/plasmic-mailchimp";
+// import { registerAll as hubspot } from "@plasmicpkgs/plasmic-hubspot";
+// import { registerAll as eventbrite } from "@plasmicpkgs/plasmic-eventbrite";
+// import { registerAll as soundcloud } from "@plasmicpkgs/plasmic-soundcloud";
+// import { registerAll as giphy } from "@plasmicpkgs/plasmic-giphy";
+// import { registerAll as spotify } from "@plasmicpkgs/plasmic-spotify";
+// import { registerAll as intercom } from "@plasmicpkgs/plasmic-intercom";
+// import { registerAll as audio } from "@plasmicpkgs/plasmic-audio";
 
 
 import { registerAll as yotpo } from "@plasmicpkgs/plasmic-yotpo";
 
 //@ts-ignore
-yotpo(PLASMIC);
-registerRevue(PLASMIC);
-ContentfulRegister(PLASMIC);
-ContentstackRegister(PLASMIC);
-registerPigeon(PLASMIC);
+// yotpo(PLASMIC);
+// registerRevue(PLASMIC);
+// ContentfulRegister(PLASMIC);
+// ContentstackRegister(PLASMIC);
+// registerPigeon(PLASMIC);
 // registerTypeform(PLASMIC);
 registerKeen(PLASMIC);
 // hubspot(PLASMIC);
-googleMaps(PLASMIC);
-eventbrite(PLASMIC);
-soundcloud(PLASMIC);
+// googleMaps(PLASMIC);
+// eventbrite(PLASMIC);
+// soundcloud(PLASMIC);
 // mailchimp(PLASMIC);
 // spotify(PLASMIC);
 
